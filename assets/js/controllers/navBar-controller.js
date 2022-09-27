52d6312e705b0bba3aaec82d1b688a2a74d3f1ae
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-09-26 23:37:30 -0300

    Navbar animation

---
+++ assets/js/controllers/navBar-controller.js
@@ -0,0 +1,30 @@
+const header = document.querySelector('header');
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo');
+const btnSubscribe = document.querySelector('.btn-subscribe');
+
+
+
+export function init() {
+
+    window.addEventListener('scroll', () => {
+        handleWindowScrollHeader();
+       
+    })
+
+
+}
+
+function handleWindowScrollHeader() {
+    let topDistance = body.getBoundingClientRect().top;
+    console.log(topDistance)
+    if(topDistance < -470){
+        logo.classList.add('show-elements');
+        btnSubscribe.classList.add('show-elements');
+        header.style.background = "rgba(4, 8, 20, 1.0)";
+    }else{
+        logo.classList.remove('show-elements');
+        btnSubscribe.classList.remove('show-elements');
+        header.style.background = "rgba(4, 8, 20, 0)";
+    }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 import * as questionsController from './controllers/questions-controller.js';
+import * as navBarController from './controllers/navBar-controller.js';
 
 questionsController.init();
+navBarController.init();
